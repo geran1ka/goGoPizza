3c3d128be1bb0cca1dfa399468aa662b518051bd
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-03-27 23:07:28 +0300

    stop video 30:00

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import { modalController } from "./modules/modalController.js";
+import { renderCart } from "./modules/renderCart.js";
 import { renderPizzas } from "./modules/renderPizzas.js";
 import { renderToppings } from "./modules/renderToppings.js";
 import { toppingsToogle } from "./modules/toppingToogle.js";
@@ -6,6 +8,14 @@ const init = () => {
   toppingsToogle();
   renderToppings();
   renderPizzas();
+  modalController({
+    modal: ".modal-cart",
+    btnOpen: ".header__cart",
+    btnClose: ".modal__close",
+    cbOpen() {
+      renderCart();
+    },
+  });
 };
 
 init();
